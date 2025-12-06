a11846b21557ab629fbd57a1960c81ab3a04ae39
JHawkZZ <jeredmcferron@gmail.com>
2025-12-05 17:52:57 -0700

    transfer

--- sram-get.bat
+++ sram-get.bat
@@ -1,3 +1,3 @@
-scp jered@192.168.50.148:~/git/super-copier-sn/*.srm ./
+scp jered@192.168.50.148:~/git/super-copier-sn/*.srm ./utils-no-github/dumped-roms/
 @echo off
 REM for recursive: scp -r ./ jered@192.168.50.148:~/git/super-copier-sn/
